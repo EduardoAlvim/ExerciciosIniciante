2f81456978df7c4cd6bb0471b27e68258a440ca6
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2023-09-18 15:40:13 -0300

    Create 1007.php

---
+++ 1007.php
@@ -0,0 +1,8 @@
+<?php
+    $A=(int)readline();
+    $B=(int)readline();
+    $C=(int)readline();
+    $D=(int)readline();
+    $DIFERENCA = ($A * $B - $C * $D);
+    printf("DIFERENCA = %d\n",$DIFERENCA)
+?>
