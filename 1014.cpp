851745f5ba2aff931d237919087ea1e8075b6b99
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2023-09-27 15:23:39 -0300

    Create 1014.cpp

---
+++ 1014.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int X;
+    double Y, R;
+    cin >> X;
+    cin >> Y;
+    R = (double)X/Y;
+    cout << fixed<<std::setprecision(3) << R << " km/l"<< endl;
+    return 0;
+}
