03db79193bb840456719ee912a791b10a644a9a9
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2023-09-27 15:37:48 -0300

    Create 1016.cpp

---
+++ 1016.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int distancia, tempo;
+    cin >> distancia;
+    tempo = (distancia*60)/30;
+    cout <<tempo<< " minutos" << endl;
+    return 0;
+}
